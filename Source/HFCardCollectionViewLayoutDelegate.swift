66c667a85db9fbf59f401ab476f0c9eb0a50cbd9
Hendrik Frahmann <mail@hendrik-frahmann.de>
2016-11-17 22:01:59 +0100

    Added options 'bottomStackedCardsMinimumScale' and 'bottomStackedCardsMaximumScale' Added some documentation

---
+++ Source/HFCardCollectionViewLayoutDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  HFCardCollectionViewLayoutDelegate.swift
+//  Pods
+//
+//  Created by Hendrik Frahmann on 17.11.16.
+//
+//
+
+import UIKit
+
+class HFCardCollectionViewLayoutDelegate: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Source/UICollectionViewExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UICollectionViewExtensions.swift
+//  Pods
+//
+//  Created by Hendrik Frahmann on 17.11.16.
+//
+//
+
+import UIKit
+
+class UICollectionViewExtensions: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
